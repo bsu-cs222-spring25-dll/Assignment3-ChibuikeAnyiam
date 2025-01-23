7232eb5bc6d8166e05f1083c629f287f9a9f9df8
anyia <anyiamchibuike3@gmail.com>
2025-01-22 22:08:12 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Leap1Year"
+
---
+++ src/test/java/SillyMethods/SillyMethods.java
@@ -0,0 +1,25 @@
+package SillyMethods;
+
+import java.util.Scanner;
+
+public class SillyMethods {
+
+    public boolean didGuess42(int i) {
+        if (i == 42){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String countTo(int i) {
+        if (i == 10){
+            return "0 1 2 3 4 5 6 7 8 9 10";
+        }
+        else if (i == 15) {
+            return "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15";
+        }
+        return "";
+    }
+}
